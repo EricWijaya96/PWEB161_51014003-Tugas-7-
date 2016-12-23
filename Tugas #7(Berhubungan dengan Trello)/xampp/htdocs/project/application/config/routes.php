a287761ed42fb32f8526d8fa358765e3b886f676
EricWijaya96 <ericwi1996@gmail.com>
2016-12-23 17:59:50 +0700

    Tampilan Form Latihan Khusus
    Andy Eric Wijaya,NIM : 51014003.
    
    -Tampilan Saja (Form Latihan Khusus).

--- Tugas #7(Berhubungan dengan Trello)/xampp/htdocs/project/application/config/routes.php
+++ Tugas #7(Berhubungan dengan Trello)/xampp/htdocs/project/application/config/routes.php
@@ -5,5 +5,7 @@ $route['default_controller'] = 'home';
 
 $route['Registrasi_Anggota'] = 'registrasi_anggota';
 
+$route['Registrasi_Latihan_Khusus'] = 'registrasi_latihan_khusus';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ Tugas #7(Berhubungan dengan Trello)/xampp/htdocs/project/application/controllers/Registrasi_Latihan_Khusus.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Registrasi_Latihan_Khusus extends CI_Controller {
+
+	public function index()
+	{
+
+		$this->load->view('registrasi_latihan_khusus');
+		
+	}
+}
