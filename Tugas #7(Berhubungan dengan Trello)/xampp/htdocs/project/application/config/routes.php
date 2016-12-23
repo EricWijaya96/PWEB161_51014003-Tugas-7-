7f19192abc71dfd89d213407cfb5965ccac8d82d
EricWijaya96 <ericwi1996@gmail.com>
2016-12-23 17:48:56 +0700

    Tampilan Form Pendaftaran Anggota
    Andy Eric Wijaya,NIM : 51014003.
    
    -Hanya Tampilan Saja (Form Pendafaran Anggota).

--- Tugas #7(Berhubungan dengan Trello)/xampp/htdocs/project/application/config/routes.php
+++ Tugas #7(Berhubungan dengan Trello)/xampp/htdocs/project/application/config/routes.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'home';
 
+$route['Registrasi_Anggota'] = 'registrasi_anggota';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
